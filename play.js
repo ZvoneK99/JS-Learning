914616d05b038bc49518ec50bac04d7a2d5c0ba2
Zvone <zvone.kozull1999@gmail.com>
2024-12-02 20:12:30 +0100

    Rest and Spread

--- play.js
+++ play.js
@@ -8,10 +8,23 @@ const person = {
 
 person.greet();
 
+const copiedPerson = {...person};
+console.log(copiedPerson);
+
+
 const hobbies = ["Nogomet", "Kuhanje"];
 /*for(let hobby of hobbies) {
     console.log(hobby);
 }*/
 
-console.log(hobbies.map(hobby => 'Hobby: '+hobby));
-console.log(hobbies);
+/*console.log(hobbies.map(hobby => 'Hobby: '+hobby));
+console.log(hobbies);*/
+//const copiedArray = hobbies.slice();
+const copiedArray = [...hobbies];
+console.log(copiedArray);
+
+const toArray = (...args) => {
+    return args;
+};
+
+console.log(toArray(1,2,3,4,5,6,7));
