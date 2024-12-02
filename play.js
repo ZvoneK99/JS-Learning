c4d6b9b818c5d8d26fd0ac355ee97a1d03fdd79f
Zvone <zvone.kozull1999@gmail.com>
2024-12-02 19:38:22 +0100

    Objects

--- play.js
+++ play.js
@@ -1,21 +1,9 @@
-const name = "Zvone";
-let age = 25;
-const hasHobbies = true;
+const person = {
+    name: 'Zvone',
+    age: 25, 
+    greet() {
+        console.log("Hi I am "+this.name);
+    }
+};
 
-age = 30;
-
-const summarizeUser = (userName, userAge, userHasHobbiy) =>{
-    this
-    return ('Ime: '+userName+" Godine: "+userAge+", Hobi: "+userHasHobbiy);
-}
-
-
-//const add = (a, b) =>  a + b;
-//const addOne = a => a+1;
-const addRandom = () => 1+2;
-
-//console.log(add(2,3));
-//console.log(addOne(1));
-console.log(addRandom());
-
-console.log(summarizeUser(name, age, hasHobbies));
+person.greet();
