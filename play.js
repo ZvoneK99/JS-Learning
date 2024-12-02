34f71f600f4abe70ee8e14d92d66a364be633ec8
Zvone <zvone.kozull1999@gmail.com>
2024-12-02 19:16:03 +0100

    Arrow functions

--- play.js
+++ play.js
@@ -3,12 +3,19 @@ let age = 25;
 const hasHobbies = true;
 
 age = 30;
-/*console.log(name);
-console.log(age);
-console.log(hasHobbies);*/
 
-function summarizeUser(userName, userAge, userHasHobbiy) {
+const summarizeUser = (userName, userAge, userHasHobbiy) =>{
+    this
     return ('Ime: '+userName+" Godine: "+userAge+", Hobi: "+userHasHobbiy);
 }
+
+
+//const add = (a, b) =>  a + b;
+//const addOne = a => a+1;
+const addRandom = () => 1+2;
+
+//console.log(add(2,3));
+//console.log(addOne(1));
+console.log(addRandom());
 
 console.log(summarizeUser(name, age, hasHobbies));
