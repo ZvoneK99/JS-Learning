afcb43101d9b48a81a38139f8b9d4bdc1dd1ea86
Zvone <zvone.kozull1999@gmail.com>
2024-12-02 19:02:15 +0100

    Reminder about JS Basics

--- play.js
+++ play.js
@@ -1,3 +1,12 @@
 var name = "Zvone";
+var age = 25;
+var hasHobbies = true;
+/*console.log(name);
+console.log(age);
+console.log(hasHobbies);*/
 
-console.log(name);
+function summarizeUser(userName, userAge, userHasHobbiy) {
+    return ('Ime: '+userName+" Godine: "+userAge+", Hobi: "+userHasHobbiy);
+}
+
+console.log(summarizeUser(name, age, hasHobbies));
