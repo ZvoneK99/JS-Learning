ca8ff2a316103d8fcb940c907e809318937c254d
Zvone <zvone.kozull1999@gmail.com>
2024-12-02 20:27:10 +0100

    Destructuring

--- play.js
+++ play.js
@@ -6,20 +6,32 @@ const person = {
     }
 };
 
+const printName = ({ name }) => {
+    console.log(name);
+}
+
+printName(person);
+
+const {name, age} = person;
+console.log(name, age);
+/*
 person.greet();
 
 const copiedPerson = {...person};
 console.log(copiedPerson);
-
+*/
 
 const hobbies = ["Nogomet", "Kuhanje"];
-/*for(let hobby of hobbies) {
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1, hobby2);
+/*
+for(let hobby of hobbies) {
     console.log(hobby);
-}*/
+}
 
-/*console.log(hobbies.map(hobby => 'Hobby: '+hobby));
-console.log(hobbies);*/
-//const copiedArray = hobbies.slice();
+console.log(hobbies.map(hobby => 'Hobby: '+hobby));
+console.log(hobbies);
+const copiedArray = hobbies.slice();
 const copiedArray = [...hobbies];
 console.log(copiedArray);
 
@@ -28,3 +40,4 @@ const toArray = (...args) => {
 };
 
 console.log(toArray(1,2,3,4,5,6,7));
+*/
